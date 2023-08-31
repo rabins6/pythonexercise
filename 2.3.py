4bb0e3d9b6cd0193b00619af270261b2b4c30e25
Rabin <rabin.shrestha@metropolia.fi>
2023-08-31 20:02:00 +0300

    exercise 2.3

---
+++ 2.3.py
@@ -0,0 +1,6 @@
+length = float(input("lenght of a rectangle : "))
+width = float(input("width of a rectanle : "))
+perimeter = 2 * length + 2 * width
+print(perimeter)
+area_of_rectangle = length * width
+print(area_of_rectangle)
