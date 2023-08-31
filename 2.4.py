15fb54cfccdc5005a82349d74bd45c67e2d176b6
Rabin <rabin.shrestha@metropolia.fi>
2023-08-31 20:23:46 +0300

    exercise 2.4

---
+++ 2.4.py
@@ -0,0 +1,13 @@
+integer1 = int(input(" type 1st number : "))
+integer2 = int(input(" type 2nd number : "))
+integer3 = int(input(" type 3rd number : "))
+
+sum_of_integers = integer1 + integer2 + integer3
+
+product_of_integers = integer1 * integer2 * integer3
+
+average_of_integers = sum_of_integers / 3
+
+print("Sum of the integer : " + str(sum_of_integers))
+print("Product of the integer : " + str(product_of_integers))
+print("Average of the integer : " + str(average_of_integers))
