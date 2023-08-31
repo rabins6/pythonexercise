0329ea7c179cba821c8a4bf7e779c343cb1658c4
Rabin <rabin.shrestha@metropolia.fi>
2023-08-31 20:27:15 +0300

    exercise 2.2 and exercise 2.3 print modify

--- 2.2.py
+++ 2.2.py
@@ -2,5 +2,5 @@ radius_str = input("Enter a radius of a circle: ")
 radius = float(radius_str)
 area_of_circle = 3.14 * radius * radius
 
-print(area_of_circle)
+print("Area of circle : " + str(area_of_circle))
 
--- 2.3.py
+++ 2.3.py
@@ -1,6 +1,7 @@
 length = float(input("lenght of a rectangle : "))
 width = float(input("width of a rectanle : "))
-perimeter = 2 * length + 2 * width
-print(perimeter)
+perimeter_of_rectangle = 2 * length + 2 * width
 area_of_rectangle = length * width
-print(area_of_rectangle)
+
+print("Perimeter of rectangle : " + str(perimeter_of_rectangle))
+print("Area of rectangle : " + str(area_of_rectangle))
