b1a267440d747839761851e79aaae918f927f118
Rabin <rabin.shrestha@metropolia.fi>
2023-09-01 12:03:22 +0300

    exercise 2.2 and exercise 2.3 print modify

---
+++ 31.py
@@ -0,0 +1,6 @@
+zander =float(input("please insert length of zander in centimeter = "))
+limit= 42-zander
+if zander >= 42:
+    print(" zander fulfill the size limit ")
+else:
+    print("The fish is", limit, "cm shorter than limit. Please release the fish back to the lake.")
