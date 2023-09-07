c084f73f780c88cbe8d856c36b3aa80d47597435
Rabin <rabin.shrestha@metropolia.fi>
2023-09-07 22:38:54 +0300

    module exercise 4 question number 2.

--- 2.6.py
+++
@@ -1,8 +0,0 @@
-import random
-
-three_digit_code = [random.randint(0, 9) for _ in range(3)]
-
-four_digit_code = [random.randint(1, 6) for _ in range(4)]
-
-print("3-Digit Code:", "".join(map(str, three_digit_code)))
-print("4-Digit Code:", "".join(map(str, four_digit_code)))
